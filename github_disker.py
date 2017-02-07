a2ec9fd0e2ebaa34a339766fd0c248f6bcd3c567
humblepride <763090709@qq.com>
2017-02-07 13:38:41 +0800

    xx

--- github_disker.py
+++ github_disker.py
@@ -2,12 +2,22 @@
 
 """
 github repo manager
+
+!+ with UI, so user can easily look through these repos +!
 """
 
+# ---------------------------------------------------------------------------
+# global var
+v_disk_root = ""
+v_github_username = ""
+v_github_password = ""
 
+
+# ---------------------------------------------------------------------------
+# repo_info
 class repo_info:
 
-    def __init__(self, name, url, desc, disk_path, topics=[], programming_lan=[], watch=0, star=0, fork=0):
+    def __init__(self, name, url, desc, disk_path, topics=[], programming_lan=[]):
         self.name = name
         self.url = url
         self.desc = desc
@@ -16,10 +26,23 @@ class repo_info:
         self.topics = topics
         self.programming_lan = programming_lan
 
+    def init_wsf(self, watch, star, fork):
         self.watch = watch
         self.star = star
         self.fork = fork
 
+    def init_lp(self, last_pull_info):
+        self.last_pull_info = last_pull_info
+
+    def init_arch(self, arch):
+        self.arch = arch
+
+    """
+    self.my_cmnt
+    """
+
+    # ---------------------------------------------------------------------------
+    # github functionality
     def watch(self):
         pass
 
@@ -29,30 +52,70 @@ class repo_info:
     def clone(self, path):
         pass
 
+    def retrive_info_wsf(self):
+        """
+            retrive repo info: watch, star, fork
+        """
+        pass
+
+    def retrive_info_lp(self):
+        pass
+
+    # ---------------------------------------------------------------------------
+    # set
     def add_topics(self, topics):
         pass
 
     def set_programming_lan(self, lan):
         pass
 
-disk_root = ""
+    # ---------------------------------------------------------------------------
+    # get
+    def check_topics(self, topics):
+        pass
 
 
+# ---------------------------------------------------------------------------
+# csv
 def load_repo_csv():
     pass
 
 
 def save_repo_csv():
+    """
+        save repo info as csv file in v_disk_root
+    """
     pass
 
 
+# ---------------------------------------------------------------------------
+# disk
 def load_repo_disk():
     pass
+
+# ---------------------------------------------------------------------------
+# search
+
+
+def search_repo_by_topic():
+    pass
+
+# ---------------------------------------------------------------------------
+# main
+
+#
+# supposed usages:
+#   1. update repos(might by some filter)
+#      check latest pull
+#      check disk has repo
+#   2. search repos
+#   3. update disk
+#
 
 
 def diff_repo_csv_disk():
     pass
 
 
-def search_repo_by_topic():
+if __name__ == "__main__":
     pass
